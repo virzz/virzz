3909463f8c0d77ef042a84e36675a4a41753c0d7
Virink <virink@outlook.com>
2020-08-06 15:02:26 +0800

    Update Framework

---
+++ cli/cmd/completion.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -7,9 +7,24 @@ import (
 	"github.com/virink/virzz/common"
 )
 
+var (
+	showVersion bool = false
+)
+
 var rootCmd = &cobra.Command{
-	Use:   common.AppName,
+	Use:   common.BinName,
 	Short: "A tools for terminal",
+	Run: func(cmd *cobra.Command, args []string) {
+		if showVersion {
+			printVersion()
+			return
+		}
+	},
+}
+
+func init() {
+	rootCmd.Flags().BoolVarP(&showVersion, "version", "V", false, "Show Version")
+	rootCmd.SuggestionsMinimumDistance = 1
 }
 
 // Execute -
