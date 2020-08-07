a055ef3591f0b1d827bdf3baf82ab47b34c91e2e
Virink <virink@outlook.com>
2020-08-08 04:31:44 +0800

    Add [ip::MAC]

--- misc/network/ip.go
+++ misc/network/ip.go
@@ -63,3 +63,41 @@ func HexToIP(s string) (string, error) {
 	}
 	return DecToIP(h)
 }
+
+// MACToDec -
+func MACToDec(s string) (string, error) {
+	_, err := net.ParseMAC(s)
+	if err != nil {
+		return "", err
+	}
+	for _, c := range []string{":", "-", "."} {
+		s = strings.ReplaceAll(s, c, "")
+	}
+	return basic.HexToDec(strings.Trim(s, ":-."))
+}
+
+// DecToMAC -
+func DecToMAC(s string) (string, error) {
+	h, err := basic.DecToHex(s)
+	if err != nil {
+		return "", err
+	}
+	h = h[2:len(h)]
+	for len(h) < 12 {
+		h = "0" + h
+	}
+	l := len(h)
+	if l == 12 || l == 16 || l == 40 {
+		r := make([]string, l/2)
+		for i := range r {
+			r[i] = h[i*2 : (i+1)*2]
+		}
+		mac := strings.Join(r, ":")
+		_, err := net.ParseMAC(mac)
+		if err != nil {
+			return "", err
+		}
+		return mac, nil
+	}
+	return "", fmt.Errorf("invalid MAC address")
+}
